78f9044acca07d39032be1fba979cd4c4b6f9f8a
Daniel Knapp <opendroid@gmail.com>
2024-04-28 16:32:29 -0400

    add Azure Cosmos lib and config, add query for root applications

--- server/index.js
+++ server/index.js
@@ -3,6 +3,25 @@ dotenv.config({
     path: ".env.server"
 })
 
+import { CosmosClient } from "@azure/cosmos";
+
+const endpoint = process.env.COSMOS_DB_ENDPOINT;
+const key = process.env.COSMOS_DB_KEY;
+
+const databaseId = process.env.COSMOS_DB_ID;
+const containerId = process.env.COSMOS_DB_CONTAINER;
+const partitionKey = { kind: 'Hash', paths: ['/partitionKey'] }
+
+const options = {
+      endpoint: endpoint,
+      key: key,
+      userAgentSuffix: 'itmd504'
+    };
+
+
+const client = new CosmosClient(options)
+const container = client.database(databaseId).container(containerId);
+
 import express from "express";
 
 const port = process.env.PORT || 5000;
@@ -10,10 +29,27 @@ const startupDate = new Date();
 
 const app = express();
 
-app.get("/api/healthcheck", (_req, res) => {
-  res.json({ message: `API is running since ${startupDate.toISOString()}` });
-});
-
 app.listen(port, () => {
   console.log("API server listening on port", port);
+});
+
+app.get("/api/healthcheck", (_req, res) => {
+  res.json({ message: `API is running since ${startupDate.toISOString()} using databaseId ${databaseId}, containerId ${containerId}` });
+});
+
+app.get("/api/applications", async (_req, res) => {
+  console.log(`requested ${_req.path}`);
+  
+  const querySpec = {
+    query: 'SELECT r FROM root r',
+    parameters: []
+  }
+
+  const { resources: results } = await container.items
+    .query(querySpec)
+    .fetchAll();
+     
+  console.log(`${_req.path} returned \n${JSON.stringify(results)}\n`)
+  res.status(200).json(results);
+  
 });
